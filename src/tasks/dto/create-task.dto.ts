1532e4cf3c1e4f7c821820549f77e423ca7b9fe1
Trong Tran <trongvn2010@gmail.com>
2019-11-30 01:42:48 +0700

    ValidationPipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,12 @@
+import {IsNotEmpty} from 'class-validator';
+
 export class CreateTaskDto {
+    @IsNotEmpty()
     title: string;
+
+    @IsNotEmpty()
     description: string;
 }
+
+// npm install class-validator class-transformer --save
+// https://github.com/typestack/class-validator
