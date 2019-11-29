b145da0cfa6165e1aa5e3eb473a2c75cb5bf720f
Trong Tran <trongvn2010@gmail.com>
2019-11-30 01:53:04 +0700

    Error handling get, delete, update

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -28,7 +28,11 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+        if(!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -44,7 +48,8 @@ export class TasksService {
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const found = this .getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
