7b721cdd8fbeff623e507e4ebb35ac16344b542c
Trong Tran <trongvn2010@gmail.com>
2019-11-30 12:19:00 +0700

    Feature signUp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+    @Post('/signup')
+    signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+        console.log(authCredentialsDto)
+        this.authService.signUp(authCredentialsDto);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository
-    ) {}
+    ) { }
+
+    signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.signUp(authCredentialsDto);
+    }
 }
