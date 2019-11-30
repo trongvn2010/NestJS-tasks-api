9681d80f36f380cf7a9cfae5653bc2bf24e58481
Trong Tran <trongvn2010@gmail.com>
2019-11-30 08:30:26 +0700

    ValidationPipe Task Filtering and Search

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,7 +12,7 @@ export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Get()
-    getAllTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    getAllTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
         if (Object.keys(filterDto).length) {
             return this.tasksService.getTasksWithFilters(filterDto);
         }
