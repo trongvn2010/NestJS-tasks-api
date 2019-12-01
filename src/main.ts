cdecbc2362d63032b540350e823c0c8afdebe334
Trong Tran <trongvn2010@gmail.com>
2019-12-01 10:48:55 +0700

    Enabling CORS from the S3 Front-end Application

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import {Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-
-  if(process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  }
-
   const serverConfig = config.get('server');
   console.log(serverConfig);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting request from orgin ${serverConfig.origin}`);
+  }
 
   const port = process.env.PORT || serverConfig.port;
 
