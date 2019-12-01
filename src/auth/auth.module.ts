29b750c7ba4be70f04e35d1c980cfee738ceabe0
Trong Tran <trongvn2010@gmail.com>
2019-12-01 09:59:06 +0700

    Applying Configuration - CodeBase

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,9 @@ import { UserRepository } from './user.repository';
 import {JwtModule} from '@nestjs/jwt';
 import {PassportModule} from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
 
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
@@ -14,9 +16,9 @@ import { JwtStrategy } from './jwt.strategy';
       defaultStrategy: 'jwt'
     }),
     JwtModule.register({
-      secret: 'topSecrect51',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600
+        expiresIn: jwtConfig.expiresIn
       }
     }),
     TypeOrmModule.forFeature([UserRepository])
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,15 +1,19 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
+
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
+    type: dbConfig.type,
+    host: process.env.RDS_HOSTNAME || dbConfig.host,
+    port: process.env.RDS_PORT ||  dbConfig.port,
     logging: false,
-    username: 'root',
-    password: '123456',
-    database: 'tasks',
+    username: process.env.RDS_USERNAME || dbConfig.username,
+    password: process.env.RDS_PASSWORD || dbConfig.password,
+    database: process.env.RDS_DB_NAME || dbConfig.database,
     entities: [
         __dirname + '/../**/*.entity.js'
     ],
-    synchronize: true
+    synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize
 }
